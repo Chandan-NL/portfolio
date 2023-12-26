75dc44990763b0ab4529d414ddf9c0c9317c8fa3
Chandan NL <cnl3475@gmail.com>
2023-12-26 19:37:17 +0530

    glich fix

--- assets/js/init.js
+++ assets/js/init.js
@@ -649,17 +649,17 @@ function currentLink(ccc, e) {
 // -------------  GLITCH  --------------------------
 // -------------------------------------------------
 
-$(".glitch").mgGlitch({
-  destroy: false,
-  glitch: true,
-  scale: true,
-  blend: true,
-  blendModeType: "hue",
-  glitch1TimeMin: 200,
-  glitch1TimeMax: 400,
-  glitch2TimeMin: 10,
-  glitch2TimeMax: 100,
-});
+// $(".glitch").mgGlitch({
+//   destroy: false,
+//   glitch: true,
+//   scale: true,
+//   blend: true,
+//   blendModeType: "hue",
+//   glitch1TimeMin: 200,
+//   glitch1TimeMax: 400,
+//   glitch2TimeMin: 10,
+//   glitch2TimeMax: 100,
+// });
 
 // -------------------------------------------------
 // -------------  RIPPLE  --------------------------
